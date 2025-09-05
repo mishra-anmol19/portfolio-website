8340c23c44a131fbae3584c5fff1e682b2bf0545
anmol.mishra <anmol.mishra@tothenew.com>
2025-09-05 15:49:43 +0530

    code fix

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -50,7 +50,7 @@ const EmailSection = () => {
       <div className="z-1">
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I'm currently looking for new opportunities, my inbox is always open.
+          I&apos;m currently looking for new opportunities, my inbox is always open.
           Whether you have a question or just want to say hi, I'll try my best
           to get back to you!
         </p>
